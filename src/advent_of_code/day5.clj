37634b1a458f0cfe17e284c349244b520055e23a
Daniel Moore <dan@Elfangor.local>
2017-12-20 13:57:28 -0500

    Add Day 5 solution

---
+++ src/advent_of_code/day5.clj
@@ -0,0 +1,35 @@
+(ns advent-of-code.day5)
+
+(def sample-maze [0 3 0 1 -3])
+(def init-maze (->> "resources/day5_input.txt"
+                    (slurp)
+                    (#(concat "[" % "]"))
+                    (apply str)
+                    (read-string)))
+
+(defn- aget-and-fn [f]
+  (fn [int-arr idx]
+    (let [val (aget int-arr idx)]
+      (aset int-arr idx (f val))
+      val)))
+
+(defn- steps-to-escape-maze [init-maze jump-rule]
+  (let [maze (int-array init-maze)
+        maze-length (count maze)]
+    (loop [index 0 step 0]
+      (if (>= index maze-length)
+        step
+        (recur (+ index (jump-rule maze index)) (inc step))))))
+
+(defn- answer-part1 [init-maze]
+  (steps-to-escape-maze init-maze (aget-and-fn inc)))
+
+(defn- answer-part2 [init-maze]
+  (steps-to-escape-maze init-maze
+                        (aget-and-fn #(if (< % 3) (inc %) (dec %)))))
+
+(assert (= (answer-part1 sample-maze) 5))
+(assert (= (answer-part2 sample-maze) 10))
+
+(answer-part1 init-maze)
+(answer-part2 init-maze)
