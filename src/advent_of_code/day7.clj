40b29a1b357d1c3dead9c547f261a2be68f53557
Daniel Moore <hello@danielmoore.xyz>
2017-12-28 17:44:44 -0500

    Reimplement and finish Day 7 using ubergraph
    I was more interested in experimenting with ubergraph in this solution than
    making it efficient, so this could easily be optimized and be made more elegant.
    The inefficiencies don't lead to any noticeable slowdown on my machine for input
    of this size, but I might go back at some point to improve performance as more
    of an academic exercise.

--- src/advent_of_code/day7.clj
+++ src/advent_of_code/day7.clj
@@ -1,29 +1,77 @@
 (ns advent-of-code.day7
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [ubergraph.core :as uber]
+            [ubergraph.alg :as alg]))
 
 (set! *warn-on-reflection* true)
 
-(defrecord Program [^String name ^int weight ^java.util.List children])
-
-(defn- note->Program [note]
+(defn- note->graph-inits [note]
   (let [[name weight children] (-> note
                                    (str/replace #"\(|\)" "")
-                                   (str/split #"\s+(:?->)?\s*" 3))]
-    (->Program name
-               (read-string weight)
-               (if-not children [] (str/split children #", ")))))
+                                   (str/split #"\s+(:?->)?\s*" 3))
+        name-keyword (keyword name)
+        program-init [name-keyword {:weight (read-string weight)}]]
+     (if-not (empty? children)
+       (conj (->> children
+                  (#(str/split % #", "))
+                  (map keyword)
+                  (map #(vector name-keyword %)))
+             program-init)
+       [program-init])))
 
-(defn parse-tower-notes [notes]
-  (map note->Program (str/split-lines notes)))
+(defn program-tree [notes]
+  (->> notes
+       (str/split-lines)
+       (map note->graph-inits)
+       (reduce concat)
+       (apply uber/digraph)))
 
-(defn- child? [potential-child program]
-  (boolean (some #{(:name potential-child)} (:children program))))
+(def PT (->> "resources/day7_input.txt" (slurp) (program-tree)))
 
-(defn- bottom-program? [program programs]
-  (not (some (partial child? program) programs)))
+(defn bottom-program [PT]
+  (first (filter (comp zero? (partial uber/in-degree PT)) (uber/nodes PT))))
 
-(defn bottom-program [programs]
-  (first (filter #(bottom-program? % programs) programs)))
+(defn- tower-weight-at [PT node]
+  (+ (uber/attr PT node :weight)
+     (reduce + (map (partial tower-weight-at PT) (uber/successors PT node)))))
 
-(def tower-data (-> "resources/day7_input.txt" (slurp) (parse-tower-notes)))
-(def day7-part1-solution (:name (bottom-program tower-data)))
+(defn- tower-weights-of-children [PT node]
+  (zipmap (uber/successors PT node)
+          (map (partial tower-weight-at PT) (uber/successors PT node))))
+
+(defn- unbalanced-child [node]
+  (->> node
+       (tower-weights-of-children PT)
+       (group-by val)
+       (map (juxt key (comp (partial map key) val)))
+       (clojure.set/map-invert)
+       (filter (fn [[keys weight]] (= (count keys) 1)))
+       (first)
+       ((fn [[skey weight]] (if (nil? weight) nil [(first skey) weight])))))
+
+(defn- highest-unbalanced-program-in-tower [node]
+  (let [bc (first (unbalanced-child node))]
+    (if bc (highest-unbalanced-program-in-tower bc) node)))
+
+(def children (uber/successors PT))
+(def predecessor (comp first (uber/predecessors PT)))
+(def siblings (comp children predecessor))
+(defn- sibling-tower-weight [node]
+  (->> node
+       (siblings)
+       (filter #(not= node %))
+       (first)
+       (tower-weight-at PT)))
+
+(defn corrected-weight [G]
+  (let [node-to-change (-> G
+                           bottom-program
+                           highest-unbalanced-program-in-tower)
+        weight-adjustment (- (sibling-tower-weight node-to-change)
+                             (tower-weight-at G node-to-change))]
+    (+ (uber/attr G node-to-change :weight) weight-adjustment)))
+
+(def day7-part1-solution (bottom-program PT))
+(def day7-part2-solution (corrected-weight PT))
+
+{:part1 day7-part1-solution :part2 day7-part2-solution}
--- test/advent_of_code/day7_test.clj
+++ test/advent_of_code/day7_test.clj
@@ -3,23 +3,20 @@
             [clojure.test :refer :all]
             [clojure.string :as str]))
 
-(deftest test-parse-tower-notes
-  (testing "The note parser correctly handles nodes with children"
-    (is (= (parse-tower-notes "foo (42) -> bar, baz")
-           (list (->Program "foo" 42 ["bar" "baz"])))))
-  (testing "The note parser correctly processes leaves"
-    (is (= (parse-tower-notes "foo (42)")
-           (list (->Program "foo" 42 []))))))
+(def sample-graph (->> ["padx (45) -> pbga, havc, qoyq"
+                        "tknk (41) -> ugml, padx, fwft"
+                        "ugml (68) -> gyxo, ebii, jptl"
+                        "fwft (72) -> ktlj, cntj, xhth"
+                        "gyxo (61)" "cntj (57)" "jptl (61)" "pbga (66)"
+                        "xhth (57)" "ebii (61)" "havc (66)" "ktlj (57)"
+                        "qoyq (66)"]
+                       (str/join "\n")
+                       (program-tree)))
 
 (deftest test-bottom-program
   (testing "The right root program is found in the sample tower"
-    (let [sample-programs (->> ["pbga (66)" "xhth (57)" "ebii (61)"
-                                "havc (66)" "ktlj (57)" "qoyq (66)"
-                                "padx (45) -> pbga, havc, qoyq"
-                                "tknk (41) -> ugml, padx, fwft" "jptl (61)"
-                                "ugml (68) -> gyxo, ebii, jptl"
-                                "fwft (72) -> ktlj, cntj, xhth" "gyxo (61)"
-                                "cntj (57)"]
-                               (str/join "\n")
-                               (parse-tower-notes))]
-      (is (= (:name (bottom-program sample-programs)) "tknk")))))
+    (is (= (bottom-program sample-graph) :tknk))))
+
+(deftest test-corrected-weight
+  (testing "The unbalanced weight is corrected properly in the sample tower"
+    (is (= (corrected-weight sample-graph) 60))))
