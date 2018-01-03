82f2e4b13a0d8c3a5f54d7082cd09605c5c6c2bd
Daniel Moore <hello@danielmoore.xyz>
2018-01-03 17:37:22 -0500

    Add Day 9, Part 2 solution

--- src/advent_of_code/day9.clj
+++ src/advent_of_code/day9.clj
@@ -4,16 +4,22 @@
 (def stream (slurp "resources/day9_input.txt"))
 
 (defn- scrub-bangs [s] (str/replace s #"!." ""))
+(defn- collect-garbage [s] (re-seq #"<([^>]*)>,?" s))
 (defn- remove-garbage [s] (str/replace s #"<[^>]*>,?" ""))
 (defn score
   ([coll] (score coll 1))
   ([coll level]
    (reduce + level (map #(score % (inc level)) coll))))
 
-(-> stream
-    (scrub-bangs)
-    (remove-garbage)
-    (str/replace #"\{" "(")
-    (str/replace #"\}" ")")
-    (read-string)
-    (score))
+(let [cleaner-stream (scrub-bangs stream)
+      clean-stream (remove-garbage cleaner-stream)
+      garbage (collect-garbage cleaner-stream)]
+
+  {:part1-solution (-> clean-stream
+                       (str/replace #"\{" "(")
+                       (str/replace #"\}" ")")
+                       (read-string)
+                       (score))
+   :part2-solution (->> garbage
+                        (map (comp count second))
+                        (reduce +))})
