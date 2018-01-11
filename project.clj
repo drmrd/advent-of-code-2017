5e6b49355cb23d145f03d81f19d74c033b88976f
Daniel Moore <hello@danielmoore.xyz>
2018-01-11 10:08:20 -0500

    Update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject advent_of_code "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Learning Clojure working through Advent of Code 2017"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
