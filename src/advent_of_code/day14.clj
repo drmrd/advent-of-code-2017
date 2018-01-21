e49753962e54d788be4126d86b355d61f380f256
Daniel Moore <hello@danielmoore.xyz>
2018-01-21 11:11:28 -0500

    Add Day 14, Part 2 solution
    I'm playing catch up on some commits from the past week. Here's my solution to
    Part 2 of Day 14.

--- src/advent_of_code/day14.clj
+++ src/advent_of_code/day14.clj
@@ -1,7 +1,10 @@
 (ns advent-of-code.day14
   (:require [advent-of-code.day10 :refer [knot-hash]]
+            [clojure.math.combinatorics :as combo]
             [clojure.pprint :refer [cl-format]]
-            [criterium.core :as criterium]))
+            [criterium.core :as criterium]
+            [ubergraph.core :as uber]
+            [ubergraph.alg :as alg]))
 
 (def key-string "wenycdww")
 
@@ -18,4 +21,28 @@
 (defn- binary-hashes [key-string]
   (->> key-string (hash-inputs) (map (comp hex-str->bit-list knot-hash))))
 
-(reduce + (flatten (binary-hashes key-string))) ; => 8226 (Part 1 Solution)
+;; (reduce + (flatten (binary-hashes key-string))) ; => 8226 (Part 1 Solution)
+
+(defn- contiguous-used-regions [key-string]
+  (let [memory (binary-hashes key-string)
+        used? (fn [[i j]] (-> memory (nth i) (nth j) (pos?)))
+        address? (fn [coords] (->> coords
+                                  (map #(and (>= % 0) (< % 128)))
+                                  (apply #(and %1 %2))))
+        used-pairs (->> 128
+                        (range)
+                        (#(combo/cartesian-product % %))
+                        (filter used?))
+        used-neighbors
+        (let [adjacency-shifts [[0 1] [0 -1] [-1 0] [1 0]]]
+          (fn [pair] (filter #(and (address? %) (used? %))
+                            (map #(map + pair %) adjacency-shifts))))
+        used-adjacent-pairs
+        (distinct (apply concat (map (fn [p] (map #(vector p %)
+                                                 (used-neighbors p)))
+                                     used-pairs)))
+        g (apply uber/graph used-adjacent-pairs (concat used-pairs
+                                                        used-adjacent-pairs))]
+    (count (alg/connected-components g))))
+
+;; (contiguous-used-regions key-string)    ; => 1128 (Part 2 Solution)
